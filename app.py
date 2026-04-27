6e197160d1e94207544f9f637e44a379f7234121
yuvakumarB <bhogeyuvakumar@gmail.com>
2026-04-27 20:19:54 +0530

    mini etl project

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+data = [
+    {"name": "A", "age": 25},
+    {"name": "B", "age": -5},   # invalid
+    {"name": "C", "age": 30}
+]
+
+@app.route("/")
+def home():
+    return "Mini ETL Running!"
+
+@app.route("/etl")
+def etl():
+    valid = []
+    invalid = []
+
+    for record in data:
+        if record["age"] >= 0:
+            valid.append(record)
+        else:
+            invalid.append(record)
+
+    return jsonify({
+        "valid_data": valid,
+        "invalid_data": invalid
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
